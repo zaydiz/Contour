3e2997476a0600dbc940cfac83e67551cadb88e7
Anton Novoselov <nantog@gmail.com>
2021-04-21 14:28:25 +0300

    Add Local notifications

--- Shiny/Activities/Projects/EditProjectView.swift
+++ Shiny/Activities/Projects/EditProjectView.swift
@@ -21,6 +21,11 @@ struct EditProjectView: View {
     @State private var color: String
     
     @State private var engine = try? CHHapticEngine()
+    
+    @State private var remindMe: Bool
+    @State private var reminderTime: Date
+    
+    @State private var showingNotificationsError = false
 
     
     let colorColumns = [
@@ -33,6 +38,14 @@ struct EditProjectView: View {
         _title = State(wrappedValue: project.projectTitle)
         _detail = State(wrappedValue: project.projectDetail)
         _color = State(wrappedValue: project.projectColor)
+        
+        if let projectReminderTime = project.reminderTime {
+            _reminderTime = State(wrappedValue: projectReminderTime)
+            _remindMe = State(wrappedValue: true)
+        } else {
+            _reminderTime = State(wrappedValue: Date())
+            _remindMe = State(wrappedValue: false)
+        }
     }
 
     var body: some View {
@@ -47,6 +60,26 @@ struct EditProjectView: View {
                     ForEach(Project.colors, id: \.self, content: colorButton)
                 }
                 .padding(.vertical)
+            }
+            
+            Section(header: Text("Project reminders")) {
+                Toggle("Show reminders", isOn: $remindMe.animation().onChange(update))
+                    .alert(isPresented: $showingNotificationsError) {
+                        Alert(
+                            title: Text("Oops!"),
+                            message: Text("There was a problem. Please check you have notifications enabled."),
+                            primaryButton: .default(Text("Check Settings"), action: self.showAppSettings),
+                            secondaryButton: .cancel()
+                        )
+                    }
+
+                if remindMe {
+                    DatePicker(
+                        "Reminder time",
+                        selection: $reminderTime.onChange(update),
+                        displayedComponents: .hourAndMinute
+                    )
+                }
             }
 
             // swiftlint:disable:next line_length
@@ -145,12 +178,41 @@ struct EditProjectView: View {
         project.title = title
         project.detail = detail
         project.color = color
+        
+        if remindMe {
+            project.reminderTime = reminderTime
+
+            dataController.addReminders(for: project) { success in
+                if success == false {
+                    project.reminderTime = nil
+                    remindMe = false
+
+                    showingNotificationsError = true
+                }
+            }
+        } else {
+            project.reminderTime = nil
+            dataController.removeReminders(for: project)
+        }
     }
     
     func delete() {
         dataController.delete(project)
         presentationMode.wrappedValue.dismiss()
     }
+    
+    
+    func showAppSettings() {
+        guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else {
+            return
+        }
+
+        if UIApplication.shared.canOpenURL(settingsUrl) {
+            UIApplication.shared.open(settingsUrl)
+        }
+    }
+    
+    
 }
 
 struct EditProjectView_Previews: PreviewProvider {
